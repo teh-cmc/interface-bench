346dbe34a3ebd93b29147de7e885e41febb88fa3
Clement Rey <cr.rey.clement@gmail.com>
2016-07-17 20:10:33 +0200

    added vcall benchmark

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// -----------------------------------------------------------------------------
+
+// Int provides an `int64` that implements the `Summable` interface.
+type Int int64
+
+// Sum simply adds two `Int`s.
+func (i Int) Sum(i2 Int) Int { return i + i2 }
+
+type Summable interface {
+	Sum(i Int) Int
+}
+
+// -----------------------------------------------------------------------------
+
+const nbOps int = 1e8
+
+func main() {
+	var start time.Time
+
+	var iConcrete Int
+	start = time.Now()
+	for i := 0; i < nbOps; i++ {
+		iConcrete = iConcrete.Sum(Int(10))
+	}
+	_ = iConcrete
+	fmt.Printf("[concrete]  computed %d sums in %v\n", nbOps, time.Now().Sub(start))
+
+	var iInterface Summable = Int(0)
+	start = time.Now()
+	for i := 0; i < nbOps; i++ {
+		iInterface = iInterface.Sum(Int(10))
+	}
+	_ = iInterface
+	fmt.Printf("[interface] computed %d sums in %v\n", nbOps, time.Now().Sub(start))
+}
